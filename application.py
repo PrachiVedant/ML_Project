f1b1aa267eea98dcea1a829c52ea0a44c61a84e2
PRACHI  VEDANT <prachivedant2005@gmail.com>
2025-07-22 08:03:39 +0530

    updates

--- application.py
+++ application.py
@@ -5,9 +5,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import PredictPipeline,CustomData
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-app=application
+application=app 
 
 # route for home page
 @app.route("/",methods = ["GET"])
